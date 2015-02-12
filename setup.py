2ec2039d5bcaf97caee80e727689ebf24b7f82de
jedelman8 <jedelman8@gmail.com>
2015-02-11 21:29:34 -0800

    testing

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='nexus_ansible',
+  packages=['nexus_ansible'],
+  version='0.1',
+  description='A random test lib',
+  author='Jason Edelman',
+  author_email='jedelman8@gmail.com',
+  url='https://github.com/jedelman8/nexus_ansible',
+  download_url='https://github.com/jedelman8/nexus_ansible/tarball/0.1',
+  install_requires=[
+      'xmltodict',
+  ],
+)
